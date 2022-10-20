4dd95e8aae499e83c0d9d1d676cd1cc9cd79f17a
Enrique del Valle Perez <enrique.delvalle@agilethought.com>
2022-10-19 19:25:07 -0500

    BoxLayout and AnchorLayout

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 
 
-class BoxLayoutExample(BoxLayout):
+class AnchorLayoutExample(AnchorLayout):
     pass
 
-    '''def __int__(self, **kwargs):
+
+class BoxLayoutExample(BoxLayout):
+    pass
+    '''def __init__(self, **kwargs):
         super().__init__(**kwargs)
         b1 = Button(text="A")
         b2 = Button(text="B")
@@ -15,7 +19,6 @@ class BoxLayoutExample(BoxLayout):
         self.add_widget(b1)
         self.add_widget(b2)
         self.add_widget(b3)'''
-
 
 
 class MainWidget(Widget):
