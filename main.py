44034b01b07cada7bb94ea44a65859c2d3486717
Enrique <edelvallep@gmail.com>
2022-10-20 23:25:16 -0500

    widgets example

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from kivy.app import App
+from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+
+
+class WidgetsExample(GridLayout):
+    my_text = StringProperty("Hello!")
+    count = 1
+    def on_button_click(self):
+        print("Button clicked")
+        self.count += 1
+        self.my_text = str(self.count)
+
+
+
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.orientation = "lr-bt"
+        for i in range(0, 100):
+            # size = dp(100)
+            size = dp(100)
+            b = Button(text=str(i+1), size_hint=(None, None), size=(size, size))
+            self.add_widget(b)
 
 
 class GridLayoutExample(GridLayout):
